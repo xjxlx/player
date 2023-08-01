8f45a94f0bab3e9261dd4acf61f2d72c65f20830
xjxliuxing <xjxliuxing@163.com>
2023-08-01 14:59:53 +0800

    version 2.0.0

--- app/src/main/java/com/apphelper/player/BaseVideoView.java
+++ app/src/main/java/com/apphelper/player/BaseVideoView.java
@@ -3,21 +3,20 @@ package com.apphelper.player;
 import android.content.Context;
 import android.util.AttributeSet;
 
-//import com.shuyu.gsyvideoplayer.app.BaseBuddyVideoPlayer;
+import com.shuyu.gsyvideoplayer.app.BaseBuddyVideoPlayer;
 
-//public class BaseVideoView extends BaseBuddyVideoPlayer {
-public class BaseVideoView  {
+public class BaseVideoView extends BaseBuddyVideoPlayer {
 
-//    public BaseVideoView(Context context, Boolean fullFlag) {
-//        super(context, fullFlag);
-//    }
-//
-//    public BaseVideoView(Context context) {
-//        super(context);
-//    }
-//
-//    public BaseVideoView(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//    }
+    public BaseVideoView(Context context, Boolean fullFlag) {
+        super(context, fullFlag);
+    }
+
+    public BaseVideoView(Context context) {
+        super(context);
+    }
+
+    public BaseVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
 }
